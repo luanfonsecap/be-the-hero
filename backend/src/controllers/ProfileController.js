48fcb8e8cae10eafe6ec4c0eca4532732a0e8b90
luanfonsecap <luanfonsecap@icloud.com>
2020-03-31 21:58:34 -0300

    melhorando rota de listagem de casos por ong

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -4,13 +4,22 @@ module.exports = {
   async index(req, res) {
     const ong_id = req.headers.authorization;
 
+    if (!ong_id) {
+      return res.status("401").json({ error: "Operation not permitted." });
+    }
+
+    const ong = await connection("ongs")
+      .where("id", ong_id)
+      .first()
+      .select("*");
+
+    if (!ong) {
+      return res.status("404").json({ error: "This ONG does not exists." });
+    }
+
     const incidents = await connection("incidents")
       .where("ong_id", ong_id)
       .select("*");
-
-    if (!incidents.length) {
-      return res.status("401").json({ error: "Operation not permitted." });
-    }
 
     return res.json(incidents);
   }
