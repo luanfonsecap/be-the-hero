6fa0eff132bf5fb1ce82340edcc3c2c7e8af2a66
luanfonsecap <luanfonsecap@icloud.com>
2020-04-01 14:59:02 -0300

    ajsute de visibilidade do modal

--- frontend/src/Components/Modal/index.js
+++ frontend/src/Components/Modal/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./styles.css";
 
-export default function Modal({ children, visibility }) {
+export default function Modal({ children, visibility = "none" }) {
   return (
     <div className="modal-shadow" style={{ display: visibility }}>
       <div className="modal-container">{children}</div>
